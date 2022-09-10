273dff18c9310fe39bf2532627be298f19f269c0
baidiwang <baidiwang30@gmail.com>
2022-09-09 23:16:25 -0400

    css changes

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,61 +1,87 @@
-import React, { useState } from 'react'
-import {connect} from 'react-redux'
-import { authenticate } from '../store'
-import pokemonLogo from '../images/pokemon_logo.png'
-import pokemonBall from '../images/pokemon_ball.png'
-import { useHistory } from 'react-router-dom'
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { authenticate } from "../store";
+import pokemonLogo from "../images/pokemon_logo.png";
+import pokemonBall from "../images/pokemon_ball.png";
+import { useHistory } from "react-router-dom";
 
 const Navbar = (props) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [showSignIn, setShowSignIn] = useState(false);
-  const {handleSubmit, error} = props
+  const { handleSubmit, error } = props;
 
   const history = useHistory();
 
   return (
     <div className="navbar">
       <img src={pokemonLogo} alt="pokemon-logo" className="pokemon-logo" />
-      {
-        !showSignIn ? (
-          <div>
-            <div className="action-wrapper">
-              <img src={pokemonBall} alt="pokemon-ball" className="pokemon-ball" />
-              <button onClick={() => setShowSignIn(true)}>Sign To Play</button>
-            </div>
-            <div className="action-wrapper">
-              <img src={pokemonBall} alt="pokemon-ball" className="pokemon-ball" />
-              <button onClick={() => history.push('/home')}>Play as a guest</button>
+      {!showSignIn ? (
+        <div>
+          <div className="action-wrapper">
+            <img
+              src={pokemonBall}
+              alt="pokemon-ball"
+              className="pokemon-ball"
+            />
+            <button onClick={() => setShowSignIn(true)}>Sign In To Play</button>
+          </div>
+          <div className="action-wrapper">
+            <img
+              src={pokemonBall}
+              alt="pokemon-ball"
+              className="pokemon-ball"
+            />
+            <button onClick={() => history.push("/home")}>
+              Play As A Guest
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="login-wrapper">
+          <div className="title">Sign In</div>
+          <div className="login-row">
+            <div className="login-row-title">User Name</div>
+            <div className="login-row-input">
+              <input
+                type="text"
+                name="username"
+                autoComplete="off"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
           </div>
-        ) : (
-          <div className="login-wrapper">
-            <div className="title">Sign In</div>
-            <div className="login-row">
-              <div className="login-row-title">User Name</div>
-              <div className="login-row-input">
-                <input type="text" name="username" autoComplete="off" value={username} onChange={e => setUsername(e.target.value)} />
-              </div>
-            </div>
-            <div className="login-row">
-              <div className="login-row-title">Password</div>
-              <div className="login-row-input">
-                <input type="password" name="password" autoComplete="new-password" value={password} onChange={e => setPassword(e.target.value)} />
-              </div>
-            </div>
-            {error && error.response && <div> {error.response.data} </div>}
-            <div className="login-row">
-              <button onClick={() => handleSubmit(username, password)}>PLAY</button>
+          <div className="login-row">
+            <div className="login-row-title">Password</div>
+            <div className="login-row-input">
+              <input
+                type="password"
+                name="password"
+                autoComplete="new-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
           </div>
-        )
-      }
+          {error && error.response && <div> {error.response.data} </div>}
+          <div className="login-row">
+            <button onClick={() => handleSubmit(username, password)}>
+              PLAY
+            </button>
+          </div>
+        </div>
+      )}
       <div>
-        <img src={pokemonBall} alt="pokemon-ball" className="pokemon-ball-big" />
+        <img
+          src={pokemonBall}
+          alt="pokemon-ball"
+          className="pokemon-ball-big"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
@@ -64,20 +90,20 @@ const Navbar = (props) => {
  *   function, and share the same Component. This is a good example of how we
  *   can stay DRY with interfaces that are very similar to each other!
  */
-const mapLogin = state => {
+const mapLogin = (state) => {
   return {
-    name: 'login',
-    displayName: 'Login',
-    error: state.auth.error
-  }
-}
+    name: "login",
+    displayName: "Login",
+    error: state.auth.error,
+  };
+};
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleSubmit(username, password) {
-      dispatch(authenticate(username, password, 'login'))
-    }
-  }
-}
+      dispatch(authenticate(username, password, "login"));
+    },
+  };
+};
 
-export default connect(mapLogin, mapDispatch)(Navbar)
+export default connect(mapLogin, mapDispatch)(Navbar);
--- script/seed.js
+++ script/seed.js
@@ -1,55 +1,58 @@
-'use strict'
+"use strict";
 
-const {db, models: {User} } = require('../server/db')
-const axios = require('axios')
-const {response} = require('express')
-const Pokemon = require('../server/db/models/Pokemons')
+const {
+  db,
+  models: { User },
+} = require("../server/db");
+const axios = require("axios");
+const { response } = require("express");
+const Pokemon = require("../server/db/models/Pokemons");
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
-  ])
+    User.create({ username: "cody", password: "123" }),
+    User.create({ username: "murphy", password: "123" }),
+  ]);
 
-  console.log(`seeded ${users.length} users`)
+  console.log(`seeded ${users.length} users`);
 
   // Get Pokemon data
-  const pokemonRequests = []
-  for (let i = 1; i < 20; i++) {
-    const request = axios.get('https://pokeapi.co/api/v2/pokemon-form/' + i)
-    pokemonRequests.push(request)
+  const pokemonRequests = [];
+  for (let i = 1; i < 100; i++) {
+    const request = axios.get("https://pokeapi.co/api/v2/pokemon-form/" + i);
+    pokemonRequests.push(request);
   }
 
-  try{
-    const responses = await axios.all(pokemonRequests)
-    for(let i = 0; i < responses.length; i++) {
-      const data = responses[i].data
+  try {
+    const responses = await axios.all(pokemonRequests);
+    for (let i = 0; i < responses.length; i++) {
+      const data = responses[i].data;
       await Pokemon.create({
         name: data.pokemon.name,
         imageUrl: data.sprites.front_default,
-        type: data.types[0].type.name
-      })
+        type: data.types[0].type.name,
+      });
     }
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-  console.log(`seeded ${pokemonRequests.length} pokemons`)
+  console.log(`seeded ${pokemonRequests.length} pokemons`);
 
-  console.log(`seeded successfully`)
+  console.log(`seeded successfully`);
   return {
     users: {
       cody: users[0],
-      murphy: users[1]
-    }
-  }
+      murphy: users[1],
+    },
+  };
 }
 
 /*
@@ -58,16 +61,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -77,8 +80,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
